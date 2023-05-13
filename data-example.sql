ea1f433778334cf51e7d4f3d286fde46a61f7d97
AgathePons <ponsagathe37@gmail.com>
2023-05-13 12:44:41 +0200

    add sql insert script for example + script to lauch it in docker container

---
+++ data-example.sql
@@ -0,0 +1,5 @@
+SET client_encoding = 'UTF8';
+
+insert into tablename(id, col_one, col_two, col_three) values (1, 'value one of col 2', 11, 111);
+insert into tablename(id, col_one, col_two, col_three) values (2, 'value two of col 2', 22, 222);
+insert into tablename(id, col_one, col_two, col_three) values (3, 'value three of col 2', 33, 333);
